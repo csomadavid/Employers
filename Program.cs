feae7d7eaf43e0ff0b4b47cc059308a381094ea2
cdvid <csomad@kkszki.hu>
2024-09-17 11:53:20 +0200

    Alapvető osztály és fájl beolvasás funkció létrehozása

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
-
-namespace Employers
+class Program
 {
     public class Alkalmazott
     {
-        public int Azonosito;
-        public string Nev;
-        public int Kor;
-        public int Kereset;
+        public int Azonosito { get; set; }
+        public string Nev { get; set; }
+        public int Kor { get; set; }
+        public decimal Kereset { get; set; }
 
-        public Alkalmazott(int azonosito, string nev, int kor, int kereset)
+        public Alkalmazott(int azonosito, string nev, int kor, decimal kereset)
         {
             Azonosito = azonosito;
             Nev = nev;
@@ -20,56 +20,93 @@ namespace Employers
             Kereset = kereset;
         }
     }
-    class Program
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string fajlPath = "tulajdonsagok_100sor.txt";
+        List<Alkalmazott> alkalmazottak = new List<Alkalmazott>();
+
+        try
         {
-            // Az alkalmazottak listájának beolvasása
-            var alkalmazottak = BeolvasAlkalmazottakat("tulajdonsagok_100sor.txt");
-
-            // Ellenőrizd az adatok betöltését
-            Console.WriteLine($"Betöltve {alkalmazottak.Count} alkalmazott");
-
-            // Az alkalmazottak nevének kiírása
-            KiirAlkalmazottakNevei(alkalmazottak);
-        }
-
-        // Adatok beolvasása a fájlból
-        static List<Alkalmazott> BeolvasAlkalmazottakat(string fajlnev)
-        {
-            var alkalmazottak = new List<Alkalmazott>();
-            try
+            foreach (var sor in File.ReadLines(fajlPath))
             {
-                foreach (var sor in File.ReadLines(fajlnev))
+                var reszek = sor.Split(';');
+
+                if (reszek.Length != 4)
                 {
-                    var darabok = sor.Split(',');
-                    if (darabok.Length == 4)
-                    {
-                        var alkalmazott = new Alkalmazott(
-                            int.Parse(darabok[0]),
-                            darabok[1],
-                            int.Parse(darabok[2]),
-                            int.Parse(darabok[3])
-                        );
-                        alkalmazottak.Add(alkalmazott);
-                    }
+                    Console.WriteLine($"Hibás sor: {sor}");
+                    continue;
+                }
+
+                try
+                {
+                    var azonosito = int.Parse(reszek[0]);
+                    var nev = reszek[1].Trim();
+                    var kor = int.Parse(reszek[2]);
+                    var kereset = decimal.Parse(reszek[3]);
+
+                    alkalmazottak.Add(new Alkalmazott(azonosito, nev, kor, kereset));
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"Formátum hiba az alábbi sorban: {sor}. Hiba: {e.Message}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Hiba az alábbi sor feldolgozása közben: {sor}. Hiba: {e.Message}");
                 }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Hiba a fájl beolvasása során: {ex.Message}");
-            }
-            return alkalmazottak;
         }
-
-        // Az alkalmazottak neveinek kiírása
-        static void KiirAlkalmazottakNevei(List<Alkalmazott> alkalmazottak)
+        catch (IOException e)
         {
-            foreach (var alkalmazott in alkalmazottak)
-            {
-                Console.WriteLine(alkalmazott.Nev);
-            }
+            Console.WriteLine($"Hiba a fájl beolvasása közben: {e.Message}");
+            return;
         }
 
+        if (alkalmazottak.Count == 0)
+        {
+            Console.WriteLine("Nincsenek alkalmazottak az adatbázisban.");
+            return;
+        }
+
+        // 3. Jelenítse meg az összes dolgozó nevét
+        Console.WriteLine("Alkalmazottak nevei:");
+        foreach (var alkalmazott in alkalmazottak)
+        {
+            Console.WriteLine(alkalmazott.Nev);
+        }
+
+        // 4. Írja ki azoknak az azonosítóját és nevét, akik a legjobban keresnek
+        try
+        {
+            var maxKereset = alkalmazottak.Max(a => a.Kereset);
+            var legmagasabbKeresetuek = alkalmazottak.Where(a => a.Kereset == maxKereset);
+
+            Console.WriteLine("\nLegmagasabb keresetű alkalmazottak:");
+            foreach (var alkalmazott in legmagasabbKeresetuek)
+            {
+                Console.WriteLine($"Azonosító: {alkalmazott.Azonosito}, Név: {alkalmazott.Nev}");
+            }
+        }
+        catch (InvalidOperationException e)
+        {
+            Console.WriteLine($"Hiba a legmagasabb kereset meghatározása közben: {e.Message}");
+        }
+
+        // 5. Írja ki azoknak a nevét és korát, akiknek 10 évük van a nyugdíjig (65 év a nyugdíj korhatár)
+        const int nyugdijKor = 65;
+        var nyugdijhozKozeliek = alkalmazottak.Where(a => (nyugdijKor - a.Kor) == 10);
+
+        Console.WriteLine("\nNyugdíjhoz közel álló alkalmazottak:");
+        foreach (var alkalmazott in nyugdijhozKozeliek)
+        {
+            Console.WriteLine($"Név: {alkalmazott.Nev}, Kor: {alkalmazott.Kor}");
+        }
+
+        // 6. Számolja meg hányan keresnek 50,000 forint felett
+        decimal keresetKuszob = 50000;
+        var magasKeresetuAlkalmazottak = alkalmazottak.Count(a => a.Kereset > keresetKuszob);
+
+        Console.WriteLine($"\n50000 forint felett kereső alkalmazottak száma: {magasKeresetuAlkalmazottak}");
     }
- }
+}
